05853363c54f4a19c559ce50d270df2dc5eb4886
MarySalemme <marysalemme1@gmail.com>
2019-02-06 18:23:58 +0000

    Use AVFoundation to play sounds

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var player: AVAudioPlayer!
+    var selectedSoundFileName = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        playSound(soundFileName: soundArray[sender.tag - 1])
     }
     
-  
+    func playSound(soundFileName : String) {
+        let soundUrl = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
 
+        do {
+            player = try AVAudioPlayer(contentsOf: soundUrl!)
+        } catch {
+            print(error)
+        }
+        
+        player.play()
+    }
+    
 }
 
